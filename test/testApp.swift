7e4f609d576c7437562b0e47cc867bc6d2c496ba
GouceQ <gouchengouceq@163.com>
2024-10-10 07:55:02 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by 勾陈 on 2024/10/10.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
